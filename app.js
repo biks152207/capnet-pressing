398f2d77b9bcc822c4549cc667005b0708e1259a
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-01 07:56:19 +0100

    [F #1] Replace simple quotes by double quotes

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var indexRoute = require('./routes/index');
-var locationRoute = require('./routes/location');
-var servicesRoute = require('./routes/services');
-var contactRoute = require('./routes/contact');
-var aboutRoute = require('./routes/about');
-var legalRoute = require('./routes/legal');
+var indexRoute = require("./routes/index");
+var locationRoute = require("./routes/location");
+var servicesRoute = require("./routes/services");
+var contactRoute = require("./routes/contact");
+var aboutRoute = require("./routes/about");
+var legalRoute = require("./routes/legal");
 
 var app = express();
 
 // View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // TODO @kla: Uncomment after placing your favicon in /dist
-// app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(favicon(path.join(__dirname, "dist", "favicon.ico")));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.use('/', indexRoute);
-app.use('/location', locationRoute);
-app.use('/services', servicesRoute);
-app.use('/contact', contactRoute);
-app.use('/about', aboutRoute);
-app.use('/legal', legalRoute);
+app.use("/", indexRoute);
+app.use("/location", locationRoute);
+app.use("/services", servicesRoute);
+app.use("/contact", contactRoute);
+app.use("/about", aboutRoute);
+app.use("/legal", legalRoute);
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -44,10 +44,10 @@ app.use(function(req, res, next) {
 
 // Development error handler
 // Will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -58,7 +58,7 @@ if (app.get('env') === 'development') {
 // No stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,82 @@
-'use strict';
+"use strict";
 
 // Load plugins
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del');
+var gulp = require("gulp"),
+    sass = require("gulp-ruby-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cssnano = require("gulp-cssnano"),
+    jshint = require("gulp-jshint"),
+    uglify = require("gulp-uglify"),
+    imagemin = require("gulp-imagemin"),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    notify = require("gulp-notify"),
+    cache = require("gulp-cache"),
+    livereload = require("gulp-livereload"),
+    del = require("del");
 
 // Styles
-gulp.task('styles', function() {
-  return sass('src/styles/main.scss', { style: 'expanded' })
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(rename({ suffix: '.min' }))
+gulp.task("styles", function() {
+  return sass("src/styles/main.scss", { style: "expanded" })
+    .pipe(autoprefixer("last 2 version"))
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(cssnano())
-    .pipe(gulp.dest('dist/styles'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest("dist/styles"))
+    .pipe(notify({ message: "Styles task complete" }));
 });
 
 // Scripts
-gulp.task('scripts', function() {
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
+gulp.task("scripts", function() {
+  return gulp.src("src/scripts/**/*.js")
+    .pipe(jshint(".jshintrc"))
+    .pipe(jshint.reporter("default"))
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("dist/scripts"))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(gulp.dest("dist/scripts"))
+    .pipe(notify({ message: "Scripts task complete" }));
 });
 
 // Images
-gulp.task('images', function() {
-  return gulp.src('src/images/**/*')
+gulp.task("images", function() {
+  return gulp.src("src/images/**/*")
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/images'))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe(gulp.dest("dist/images"))
+    .pipe(notify({ message: "Images task complete" }));
 });
 
 // Clean
-gulp.task('clean', function() {
-  return del(['dist/styles', 'dist/scripts', 'dist/images']);
+gulp.task("clean", function() {
+  return del(["dist/styles", "dist/scripts", "dist/images"]);
 });
 
 // Default task
-gulp.task('default', ['clean'], function() {
-  gulp.start('styles');
-  gulp.start('scripts');
-  gulp.start('images');
-  gulp.start('watch');
+gulp.task("default", ["clean"], function() {
+  gulp.start("styles");
+  gulp.start("scripts");
+  gulp.start("images");
+  gulp.start("watch");
 });
 
 // Watch
-gulp.task('watch', function() {
+gulp.task("watch", function() {
   // Watch .scss files
-  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch("src/styles/**/*.scss", ["styles"]);
 
   // Watch .js files
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch("src/scripts/**/*.js", ["scripts"]);
 
   // Watch image files
-  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch("src/images/**/*", ["images"]);
 
   // Watch ejs files
-  gulp.watch('views/**/*', ['scripts', 'images']);
+  gulp.watch("views/**/*", ["scripts", "images"]);
 
   // Create LiveReload server
   livereload.listen();
 
   // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**', 'views/**/*']).on('change', livereload.changed);
+  gulp.watch(["dist/**", "views/**/*"]).on("change", livereload.changed);
 });
