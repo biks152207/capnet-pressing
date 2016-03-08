f94d6f79f1b56142294c0b3ceb054d44e7d3a729
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-08 07:38:18 +0100

    [F #1] Move `app.js` in the folder `src`

--- src/app.js
+++ src/app.js
@@ -5,17 +5,17 @@ var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 
-var indexRoute = require("./routes/index");
-var locationRoute = require("./routes/location");
-var servicesRoute = require("./routes/services");
-var contactRoute = require("./routes/contact");
-var aboutRoute = require("./routes/about");
-var legalRoute = require("./routes/legal");
+var indexRoute = require("../routes/index");
+var locationRoute = require("../routes/location");
+var servicesRoute = require("../routes/services");
+var contactRoute = require("../routes/contact");
+var aboutRoute = require("../routes/about");
+var legalRoute = require("../routes/legal");
 
 var app = express();
 
 // View engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 // TODO @kla: Uncomment after placing your favicon in /dist
@@ -24,7 +24,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use("/", indexRoute);
 app.use("/location", locationRoute);
