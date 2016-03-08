4781f3f7c80ddb818e728668d285d1e250b9ef78
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-08 08:26:58 +0100

    [F #1] App.js: Add `use strict` and add default parameters to app.use method to be more explicit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require("express");
 var path = require("path");
 var favicon = require("serve-favicon");
@@ -24,7 +26,7 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use("/", express.static(path.join(__dirname, "../public")));
 
 app.use("/", indexRoute);
 app.use("/location", locationRoute);
