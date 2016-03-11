507f297b1de2ead3ab35e21ef806fa9c0ee5f0b8
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-11 08:45:41 +0100

    [F #1] Locations listing: Add `formatHours` method

--- src/scripts/modules/locations/listing.js
+++ src/scripts/modules/locations/listing.js
@@ -25,6 +25,34 @@
     this.getLocations = function () {
       return _locations;
     }
+
+    /**
+     * formatHours: Format the hours from [08:00, 16:00] to "from 8am to 4pm"
+     * @param  {Array} hours [Array containing the opening, lunch and closing hours]
+     * @return {String}      [Sentence describing the hours of the dry cleaning]
+     */
+    this.formatHours = function (hours) {
+
+      var nb_hours = hours.length,
+          opening_hour = hours[0],
+          lunch_start_hour = "",
+          lunch_end_hour = "",
+          closing_hour = hours[hours.length - 1],
+          sentence = "";
+
+      if (nb_hours === 2) {
+
+        sentence = "de " + opening_hour + " à " + closing_hour;
+      }
+      else if (nb_hours === 4) {
+
+        lunch_start_hour = hours[1];
+        lunch_end_hour = hours[2];
+        sentence = "de " + opening_hour + "-" + lunch_start_hour + " et " + lunch_end_hour + "-" + closing_hour;
+      }
+
+      return sentence;
+    }
   }
 
   app.directive("locationsListing", function () {
