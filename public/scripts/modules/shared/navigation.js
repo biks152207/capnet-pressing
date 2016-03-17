089d47287686583a2397804c3054fc33d58eaf80
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-17 18:18:22 +0100

    [F #10] Navigation: Add “active” CSS class

--- public/scripts/modules/shared/navigation.js
+++ public/scripts/modules/shared/navigation.js
@@ -5,9 +5,20 @@
 
   /**
    * navigationController
+   * @param {Object} $state
    */
-  function navigationController () {
+  function navigationController ($state) {
 
+    /**
+     * Get active CSS class: Highlight the current item in the navigation.
+     * @param  {String} [URL of the item in the navigation]
+     * @return {Object} [CSS class "active" if the item is selected]
+     */
+    this.getClass = function (link) {
+      return {
+        active: $state.current.url === link
+      };
+    }
   }
 
   app.directive("navigation", function () {
