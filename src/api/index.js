58569c34d6a9cf7623af0229b6881d0960222fa6
Kévin Lanceplaine <k.lanceplaine@talentoday.com>
2016-03-18 16:33:47 +0100

    [F #50] Contact form: Set up SendGrid

--- src/api/index.js
+++ src/api/index.js
@@ -6,11 +6,60 @@
 
   var api = express.Router();
 
+  require("dotenv").config();
+
+  var sendgrid  = require("sendgrid")(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+
+  // Locations listing #############################################################################
   api.get("/locations", function (req, res) {
     res.json({ locations: locations });
   })
 
+  // Contact form ##################################################################################
   api.post("/contact", function (req, res) {
+
+    // Compose email -----------------------------------------------------------
+    var lastName = req.body.lastName,
+        firstName = req.body.firstName,
+        email = req.body.email,
+        phone = req.body.phone,
+        address = req.body.address,
+        message = req.body.message,
+        emailSenderName = "",
+        emailSubject = "",
+        emailBody = "";
+
+    emailSenderName = firstName + " " + lastName;
+
+    emailSubject = "Nouveau message de " + emailSenderName;
+
+    emailBody = "Prénom : " + firstName + "<br>";
+    emailBody += "Nom : " + lastName + "<br>";
+    emailBody += "Email : " + email + "<br>";
+
+    if (phone) {
+      emailBody += "Téléphone : " + phone + "<br>";
+    }
+
+    if (address) {
+      emailBody += "<br>Adresse :<br>" + address + "<br>";
+    }
+
+    emailBody += "<br>Message :<br>" + message;
+
+    // Send email --------------------------------------------------------------
+    sendgrid.send({
+      to:       "kevin@lanceplaine.com",
+      from:     email,
+      fromname: emailSenderName,
+      subject:  emailSubject,
+      text:     message,
+      html:     emailBody
+    }, function (err, json) {
+      if (err) { return console.error(err); }
+      console.log(json);
+    });
+
     res.sendStatus(200);
   })
 
--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 ;(function () {
   "use strict";
 
-  var express = require("express");
-  var path = require("path");
-  var favicon = require("serve-favicon");
-  var logger = require("morgan");
-  var cookieParser = require("cookie-parser");
-  var bodyParser = require("body-parser");
-  var api = require("./api");
-  var router = require("../routes/index");
+  var express = require("express"),
+      path = require("path"),
+      favicon = require("serve-favicon"),
+      logger = require("morgan"),
+      cookieParser = require("cookie-parser"),
+      bodyParser = require("body-parser"),
+      api = require("./api"),
+      router = require("../routes/index");
 
   var app = express();
 
